23814cf0ab8c65476c391ae01cccd2ec90ae0b4e
nisalmissaka <wishwakanisal@gmail.com>
2026-01-20 13:43:46 +0530

    fix SeatRepository generics and add DB row lock method

--- src/main/java/edu/example/repository/Seatrepository.java
+++ src/main/java/edu/example/repository/Seatrepository.java
@@ -1,6 +1,6 @@
 package edu.example.repository;
 
-import edu.example.entity.Seat;
+import edu.example.model.Seat;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
