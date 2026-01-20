056de741e22757a8408a67551d9836fb95879acd
nisalmissaka <wishwakanisal@gmail.com>
2026-01-20 11:44:34 +0530

    create to SeartServiceImpl class

--- src/main/java/edu/example/service/impl/SeartServiceImpl.java
+++ src/main/java/edu/example/service/impl/SeartServiceImpl.java
@@ -1,4 +1,20 @@
 package edu.example.service.impl;
 
-public class SeartServiceImpl {
+import edu.example.entity.Seat;
+import edu.example.repository.Seatrepository;
+import edu.example.service.SeartService;
+
+public class SeartServiceImpl implements SeartService {
+
+    private final Seatrepository seatrepository;
+
+    public SeartServiceImpl(Seatrepository seatrepository) {
+        this.seatrepository = seatrepository;
+    }
+
+    @Override
+    public Seat holdSeat(Long seatId, Long userID) {
+        return null;
+    }
 }
+
