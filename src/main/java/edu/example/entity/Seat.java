ce55f5b809552d3b591ba7fc3d598001d9b77e08
nisalmissaka <wishwakanisal@gmail.com>
2026-01-20 11:18:16 +0530

    implement to seat entity class

---
+++ src/main/java/edu/example/entity/Seat.java
@@ -0,0 +1,28 @@
+package edu.example.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+@Table(name = "seats")
+public class Seat {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String SeatNumber;
+    private Long heldByUserId;
+    private int version;
+    private LocalDateTime holdExpiryTimestamp;
+
+}
