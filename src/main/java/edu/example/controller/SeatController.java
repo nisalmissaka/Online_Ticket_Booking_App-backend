0ea270b42b0423779d01a5614573bf715c836fbe
nisalmissaka <wishwakanisal@gmail.com>
2026-01-20 13:28:30 +0530

    implement to getAllSeats method

---
+++ src/main/java/edu/example/controller/SeatController.java
@@ -0,0 +1,21 @@
+package edu.example.controller;
+
+import edu.example.model.Seat;
+import edu.example.service.SeatService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/seat")
+@RequiredArgsConstructor
+@CrossOrigin
+public class SeatController {
+    private final SeatService seatService;
+
+    @GetMapping
+    public List<Seat> getAllSeats(){
+        return seatService.getAllSeats();
+    }
+}
--- src/main/java/edu/example/service/SeatService.java
+++ src/main/java/edu/example/service/SeatService.java
@@ -1,9 +1,13 @@
 package edu.example.service;
 
-import edu.example.entity.Seat;
+import edu.example.model.Seat;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public interface SeartService {
+public interface SeatService {
     Seat holdSeat (Long seatId , Long userID);
+
+    List<Seat> getAllSeats();
 }
